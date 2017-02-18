3a56866b30e0624ecc6680dfae9b89fd44e66473
Nick Harbaugh <njharb@gmail.com>
2017-02-18 15:19:28 -0600

    First commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent("p5canvas");
+}
+
+function draw() {
+    background(0);
+    fill(255);
+    ellipse(200,200,100);
+}
